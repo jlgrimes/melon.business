f4e441ff180567188406316a624ba0162b63a1da
Jared Grimes <jaredlgrimes@hotmail.com>
2023-05-08 21:29:55 +0200

    removed unfunny things

--- src/components/HomePage.tsx
+++ src/components/HomePage.tsx
@@ -3,20 +3,11 @@ import { Container, Heading, Stack } from '@chakra-ui/react';
 export const HomePage = () => {
   return (
     <Container padding={8}>
-      <Stack spacing={16}>
+      <Stack spacing={8}>
         <Stack spacing={1}>
           <Heading size='3xl'>melon 🍈</Heading>
           <Heading size='xl' color='gray.500'>
             melon business.
-          </Heading>
-        </Stack>
-        <Stack spacing={1}>
-          <Heading size='2xl'>CEO Jared Grimes</Heading>
-          <Heading size='lg' color='gray.500'>
-            Yeah, I own this company.
-          </Heading>
-          <Heading size='lg' color='gray.500'>
-            What are YOU gonna do about it?
           </Heading>
         </Stack>
         <Stack spacing={1}>
