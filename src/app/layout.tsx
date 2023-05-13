2f7fffb666f55281d2b4d76c189da5d84ccdaecc
Jared Grimes <jaredlgrimes@hotmail.com>
2023-05-12 22:28:02 -0400

    try the stretchy thing w offset

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,23 @@
-import { font } from '@/styles/fonts'
-import './globals.css'
+import { font } from '@/styles/fonts';
+import './globals.css';
 
 export const metadata = {
   title: 'melon',
   description: 'melon business',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
+      <meta
+        name='viewport'
+        content='width=device-width, initial-scale=1.0, viewport-fit=cover'
+      ></meta>
       <body className={font.className}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,6 @@ import { font } from '@/styles/fonts';
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-  styles: {
-    global: () => ({
-      body: {
-        bg: '#f4a261',
-      }
-    })
-  },
   fonts: {
     body: font.style.fontFamily,
     heading: font.style.fontFamily,
